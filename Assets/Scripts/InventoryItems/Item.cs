cc0010c8744a78ca67582ed3a20cf75fe70b0db1
Grant King <alpharock6@gmail.com>
2018-08-11 16:57:41 -0700

    Reworked platforms to use Platform Effector 2D,

---
+++ Assets/Scripts/InventoryItems/Item.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour {
+
+    public bool consumable;
+    public Sprite menuSprite;
+    public GameObject inGamePrefab;
+
+    public bool IsConsumable() {
+        return consumable;
+    }
+
+    public GameObject GetGamePrefab() {
+        return inGamePrefab;
+    }
+
+    public Sprite GetMenuSprite() {
+        return menuSprite;
+    }
+
+    public abstract bool CanUse();
+    public abstract void Use();
+
+    public override bool Equals(object other) {
+        if (other == null) {
+            return false;
+        }
+
+        Item otherItem = other as Item;
+        if (otherItem == null) {
+            return false;
+        }
+
+        return this.inGamePrefab == otherItem.inGamePrefab;
+    }
+
+    public override int GetHashCode() {
+        return base.GetHashCode();
+    }
+
+    public override string ToString() {
+        return base.ToString();
+    }
+}
